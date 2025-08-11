6edec9b568e4d1e2deeb6e09f5eb8135cd7dcb33
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-11 14:42:27 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
-// tailwind.config.cjs
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#A13500',
-        accent: '#FF8345',
-        'coke-red': '#E41B17',
-        gold: '#D4AF37'
-      }
-    },
+    extend: {},
   },
   plugins: [],
-};
+}

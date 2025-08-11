5f5eb2eb063238d6b44583f8878ae151ada36d29
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-11 11:45:39 -0300

    Update and rename CloseIcons.tsx to CloseIcon.tsx

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -5,27 +5,30 @@ type Props = {
   height?: number | string;
   className?: string;
   onClick?: () => void;
-  title?: string;
 };
 
-export default function CloseIcon({ width = 20, height = 20, className = "", onClick, title = "Fechar" }: Props) {
+export default function CloseIcon({
+  width = 20,
+  height = 20,
+  className,
+  onClick,
+}: Props) {
   return (
     <svg
       onClick={onClick}
-      className={className}
+      xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
       viewBox="0 0 24 24"
-      role="img"
-      aria-label={title}
-      xmlns="http://www.w3.org/2000/svg"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      className={className}
+      role="img"
+      aria-label="close"
     >
-      <title>{title}</title>
       <line x1="18" y1="6" x2="6" y2="18" />
       <line x1="6" y1="6" x2="18" y2="18" />
     </svg>

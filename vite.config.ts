ff24633f91a6cafd6e5a62ce02f3e2306c526bee
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-11 15:53:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Vite config minimal e compatível com Vercel
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
   },
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
+  base: "./"
 });

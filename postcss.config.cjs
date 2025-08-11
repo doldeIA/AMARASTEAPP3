131495f5819a55fc98cda499029b450fee79dbde
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-11 16:43:16 -0300

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
-// postcss.config.cjs — CommonJS format required by Node when package.json is NOT type:module
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    autoprefixer: {}
+  }
 };

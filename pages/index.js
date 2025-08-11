4c15a975fe584c7750f551e10e60a843cf9fc563
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-11 10:48:15 -0300

    Update Pages

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500 text-white">
+      <h1 className="text-5xl font-bold">Amaraste App 🚀</h1>
+    </main>
+  )
+}
